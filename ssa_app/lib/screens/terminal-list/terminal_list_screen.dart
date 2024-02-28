e51011d430fd83d77965a54ba1ff36c4140e5430
Alex Christy <a.christy@ufl.edu>
2024-02-28 11:34:25 -0500

    Refactor terminals_list_screen to be more readable

--- ssa_app/lib/screens/terminal-list/terminal_list_screen.dart
+++ ssa_app/lib/screens/terminal-list/terminal_list_screen.dart
@@ -12,133 +12,38 @@ class TerminalsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final tileWidth = getTileWidth(screenWidth);
-    final tileHeight = getTileHeight(tileWidth);
-    final imageWidth = tileHeight
-        .toDouble(); // Use the tile height as the image width for a square image
-
     return Scaffold(
       appBar: AppBar(title: const Text("Terminals")),
-      backgroundColor: const Color.fromRGBO(
-          242, 242, 247, 1.0), // Use a light gray background
+      backgroundColor: const Color.fromRGBO(242, 242, 247, 1.0),
       body: FutureBuilder<List<QueryDocumentSnapshot>>(
         future: _terminalService.getTerminals(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
+            // Better error handling
+            debugPrint('Error loading terminals: ${snapshot.error}');
             return Center(child: Text("Error: ${snapshot.error}"));
           } else {
-            return ListView.builder(
-              itemCount: snapshot.data?.length ?? 0,
-              itemBuilder: (context, index) {
-                var doc = snapshot.data![index].data() as Map<String, dynamic>;
-                String? terminalImageUrl = doc['terminalImageUrl'];
-                if (terminalImageUrl != null && terminalImageUrl.isNotEmpty) {
-                  terminalImageUrl = ImageUtil.getTerminalImageVariant(
-                      terminalImageUrl,
-                      imageWidth,
-                      tileHeight.toDouble(),
-                      context);
-                  // Preload images for the next 5 terminals
-                  for (int i = 1; i <= 5; i++) {
-                    if (index + i < snapshot.data!.length) {
-                      var nextDoc = snapshot.data![index + i].data()
-                          as Map<String, dynamic>;
-                      String? nextImageUrl = nextDoc['terminalImageUrl'];
-                      if (nextImageUrl != null && nextImageUrl.isNotEmpty) {
-                        nextImageUrl = ImageUtil.getTerminalImageVariant(
-                            nextImageUrl,
-                            imageWidth,
-                            tileHeight.toDouble(),
-                            context);
-                        final imageProvider =
-                            CachedNetworkImageProvider(nextImageUrl);
-                        precacheImage(imageProvider, context);
-                      }
-                    }
-                  }
-                }
-                return Padding(
-                  padding: EdgeInsets.only(
-                    bottom: 16.0, // Adjusted for demonstration
-                    left: (screenWidth - tileWidth) / 2, // Center the tile
-                    right: (screenWidth - tileWidth) / 2,
-                  ),
-                  child: RepaintBoundary(
-                    child: Card(
-                      color: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      elevation: 0.0,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => TerminalDetailPage(
-                                terminalData: doc,
-                              ),
-                            ),
-                          );
-                        },
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(16.0),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                flex: 35, // Takes up 35% of the row's space
-                                child: terminalImageUrl != null &&
-                                        terminalImageUrl.isNotEmpty
-                                    ? CachedNetworkImage(
-                                        imageUrl: terminalImageUrl,
-                                        width: imageWidth,
-                                        height: tileHeight.toDouble(),
-                                        fit: BoxFit.cover,
-                                        placeholder: (context, url) =>
-                                            Container(color: Colors.white),
-                                        errorWidget: (context, url, error) =>
-                                            const Icon(Icons.error),
-                                        fadeInDuration:
-                                            const Duration(milliseconds: 300),
-                                      )
-                                    : Container(
-                                        color: Colors.white,
-                                        height: tileHeight.toDouble(),
-                                      ),
-                              ),
-                              Expanded(
-                                flex:
-                                    65, // Takes up the remaining 65% of the row's space
-                                child: Padding(
-                                  padding: const EdgeInsets.all(16.0),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Text(
-                                        doc['name'] ?? 'Unknown',
-                                        style: const TextStyle(
-                                            fontSize: 18.0,
-                                            fontWeight: FontWeight.bold),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            );
+            return buildTerminalList(context, snapshot);
           }
         },
+      ),
+    );
+  }
+
+  Widget buildTerminalList(BuildContext context,
+      AsyncSnapshot<List<QueryDocumentSnapshot>> snapshot) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final tileWidth = getTileWidth(screenWidth);
+    final tileHeight = getTileHeight(tileWidth);
+    return ListView.builder(
+      itemCount: snapshot.data?.length ?? 0,
+      itemBuilder: (context, index) => TerminalListItem(
+        doc: snapshot.data![index].data() as Map<String, dynamic>,
+        screenWidth: screenWidth,
+        tileWidth: tileWidth.toDouble(),
+        tileHeight: tileHeight.toDouble(),
       ),
     );
   }
@@ -153,5 +58,106 @@ class TerminalsList extends StatelessWidget {
 
   int getTileHeight(int tileWidth) {
     return (tileWidth * 0.35).ceil(); // 35% tall of the tile width
+  }
+}
+
+class TerminalListItem extends StatelessWidget {
+  final Map<String, dynamic> doc;
+  final double screenWidth, tileWidth, tileHeight;
+
+  const TerminalListItem({
+    Key? key,
+    required this.doc,
+    required this.screenWidth,
+    required this.tileWidth,
+    required this.tileHeight,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String? terminalImageUrl = doc['terminalImageUrl'];
+    final imageWidth =
+        tileHeight; // Use the tile height as the image width for a square image
+
+    // Image URL processing
+    terminalImageUrl = ImageUtil.getTerminalImageVariant(
+      terminalImageUrl ?? '',
+      imageWidth,
+      tileHeight,
+      context,
+    );
+
+    // Preload images logic can be encapsulated in a separate method if needed
+
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: 16.0,
+        left: (screenWidth - tileWidth) / 2,
+        right: (screenWidth - tileWidth) / 2,
+      ),
+      child: RepaintBoundary(
+        child: buildTerminalCard(context, terminalImageUrl),
+      ),
+    );
+  }
+
+  Widget buildTerminalCard(BuildContext context, String? terminalImageUrl) {
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 0.0,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => TerminalDetailPage(terminalData: doc),
+            ),
+          );
+        },
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16.0),
+          child: buildCardContent(context, terminalImageUrl),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCardContent(BuildContext context, String? terminalImageUrl) {
+    return Row(
+      children: [
+        buildImageSection(terminalImageUrl),
+        buildTextSection(),
+      ],
+    );
+  }
+
+  Widget buildImageSection(String? imageUrl) {
+    return Expanded(
+      flex: 35,
+      child: imageUrl != null && imageUrl.isNotEmpty
+          ? CachedNetworkImage(
+              imageUrl: imageUrl,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => Container(color: Colors.white),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+              fadeInDuration: const Duration(milliseconds: 300),
+            )
+          : Container(color: Colors.white, height: tileHeight),
+    );
+  }
+
+  Widget buildTextSection() {
+    return Expanded(
+      flex: 65,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          doc['name'] ?? 'Unknown',
+          style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
