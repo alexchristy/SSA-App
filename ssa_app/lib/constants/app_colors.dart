7b17653e655eb70ac23c6cb1df043ccd2ffdc296
Alex Christy <a.christy@ufl.edu>
2024-02-28 11:34:13 -0500

    Split out colors and themes into seperate files

---
+++ ssa_app/lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color ghostWhite = Color.fromARGB(255, 242, 247, 255);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color greenBlue = Color.fromARGB(255, 105, 180, 255);
+  static const Color paynesGray = Color.fromARGB(255, 109, 133, 255);
+  static const Color prussianBlue = Color.fromARGB(255, 51, 85, 255);
+}
--- ssa_app/lib/theme/app_theme.dart
+++ ssa_app/lib/theme/app_theme.dart
@@ -1,22 +1,26 @@
-// lib/theme/app_theme.dart
 import 'package:flutter/material.dart';
+import '../constants/app_colors.dart'; // Adjust the import path based on your project structure
 
 class AppTheme {
   static ThemeData get lightTheme {
-    // Define the light theme
+    // Define the light theme using custom colors
     return ThemeData(
-      primarySwatch: Colors.blue,
-      cardColor: Colors.white,
+      primaryColor: AppColors.prussianBlue, // Use custom color for primaryColor
+      scaffoldBackgroundColor: AppColors.ghostWhite, // Custom background color
+      cardColor: AppColors.white,
+      appBarTheme: const AppBarTheme(
+        color: AppColors.paynesGray, // Custom AppBar color
+      ),
+      // You can define other customizations using AppColors
+      buttonTheme: const ButtonThemeData(
+        buttonColor: AppColors.greenBlue, // Custom button color
+      ),
+      textTheme: const TextTheme(
+        // Define text theme styles using AppColors if needed
+        displayLarge: TextStyle(color: AppColors.prussianBlue),
+        bodyLarge: TextStyle(color: AppColors.paynesGray),
+      ),
       // Add other theme customizations here
-    );
-  }
-
-  // If you plan to support a dark theme in the future, you can also define it here
-  static ThemeData get darkTheme {
-    // Define the dark theme
-    return ThemeData(
-      brightness: Brightness.dark,
-      // Additional dark theme customizations
     );
   }
 }
