fe7d37abf08be4e278e8d50f75d72a9c23f28e23
Alex Christy <a.christy@ufl.edu>
2024-04-03 10:12:29 -0400

    Working error for disconnection in search screen.

--- ssa_app/lib/screens/terminal-search/terminal_search_screen.dart
+++ ssa_app/lib/screens/terminal-search/terminal_search_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:ssa_app/constants/app_colors.dart';
 import 'package:algolia_helper_flutter/algolia_helper_flutter.dart';
-import 'package:ssa_app/screens/terminal-search/search_metadata.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:ssa_app/screens/terminal-search/terminal_search_result.dart';
 import 'package:ssa_app/screens/terminal-search/hits_page.dart';
 import 'package:ssa_app/utils/terminal_utils.dart';
-import 'package:ssa_app/screens/terminal-detail-page/terminal_detail_screen.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class TerminalSearchScreen extends StatefulWidget {
   final TerminalService terminalService;
@@ -35,9 +34,6 @@ class TerminalSearchScreenState extends State<TerminalSearchScreen>
       applicationID: 'TO5YM9V5TU',
       apiKey: 'be3d7f96977a9780ffc8cf2c0ea9250d',
       indexName: 'Terminals');
-
-  Stream<SearchMetadata> get _searchMetadata =>
-      _terminalSearcher.responses.map(SearchMetadata.fromResponse);
 
   final PagingController<int, TerminalSearchResult> _pagingController =
       PagingController(firstPageKey: 0);
@@ -257,11 +253,98 @@ class TerminalSearchScreenState extends State<TerminalSearchScreen>
     );
   }
 
+  Widget searchError() {
+    return Center(
+      child: Padding(
+        padding:
+            const EdgeInsets.all(16.0), // Add some padding around the content
+        child: Column(
+          mainAxisSize: MainAxisSize.min, // Use minimum space
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.signal_wifi_off,
+                size: 75.0, color: Colors.orange), // A less alarming icon
+            const SizedBox(height: 20), // Add some space between icon and text
+            const Text(
+              'Oops! Something went wrong.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black54,
+              ),
+            ),
+            const SizedBox(height: 10), // Space before the detailed message
+            Text(
+              'We had trouble fetching the data. Check your internet connection or try again later.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey.shade600,
+              ),
+            ),
+            const SizedBox(height: 20), // Space before the action button
+            ElevatedButton(
+              onPressed: () async {
+                // Check for internet connectivity before attempting to refresh
+                bool hasConnection =
+                    await InternetConnectionChecker().hasConnection;
+
+                if (!hasConnection) {
+                  // Use mounted to check if the widget is still in the widget tree
+                  if (mounted) {
+                    // If there's no internet connection, show a snackbar or some other UI indication
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text(
+                            'No internet connection. Please check your connection and try again.'),
+                        duration: Duration(seconds: 2),
+                      ),
+                    );
+                  }
+                  return; // Exit the function to prevent further execution
+                }
+                _pagingController.refresh();
+
+                // Explicitly re-trigger the search with the current search term, if needed
+                // This is just a conceptual approach; adapt it to your actual search triggering logic
+                final searchText = _searchTextController.text;
+                if (searchText.isNotEmpty) {
+                  _terminalSearcher.applyState(
+                    (state) => state.copyWith(query: searchText, page: 0),
+                  );
+                } else {
+                  // If no search text, make sure to fetch initial data if that's the intended behavior
+                  _terminalSearcher.applyState(
+                    (state) => state.copyWith(query: "", page: 0),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.blue, // Button color
+                backgroundColor: Colors.white, // Text color
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 32.0, vertical: 12.0), // Increase button size
+                textStyle: const TextStyle(
+                    fontSize: 18), // Ensure text style consistency
+              ),
+              child: const Text(
+                'Try Again',
+                style: TextStyle(fontSize: 18), // Set the text font size to 18
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   Widget _hits(BuildContext context) {
     return PagedSliverList<int, TerminalSearchResult>.separated(
       pagingController: _pagingController,
       builderDelegate: PagedChildBuilderDelegate<TerminalSearchResult>(
         itemBuilder: (context, item, index) => terminalResult(context, item),
+        firstPageErrorIndicatorBuilder: (_) => searchError(),
         noItemsFoundIndicatorBuilder: (_) => const Center(
           child: Text(
             'No results found.',
---
+++ ssa_app/lib/utils/internet_utils.dart
@@ -0,0 +1,1 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
